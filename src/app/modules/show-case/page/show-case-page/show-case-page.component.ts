5edea3cfdc44f10de5f94c63ce37d8ecd1d92cc2
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-12-09 03:53:28 -0300

    update show case component

--- src/app/modules/show-case/page/show-case-page/show-case-page.component.ts
+++ src/app/modules/show-case/page/show-case-page/show-case-page.component.ts
@@ -1,4 +1,11 @@
-import { loadItems } from './../../../../state/actions/items.actions';
+import { ItemModel } from './../../../../core/models/Item.interface';
+import { ShowCaseService } from './../../services/show-case.service';
+import { selectitemsLoading } from './../../../../state/selectors/itesm.selectors';
+import { Observable } from 'rxjs';
+import {
+  loadItems,
+  loadItemsSuccess,
+} from './../../../../state/actions/items.actions';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
@@ -8,9 +15,21 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./show-case-page.component.css'],
 })
 export class ShowCasePageComponent implements OnInit {
-  constructor(private _store: Store<any>) {}
+  loading$: Observable<boolean> = new Observable();
+  error$: Observable<any> = new Observable();
+
+  constructor(
+    private _store: Store<any>,
+    private _showCaseSvc: ShowCaseService
+  ) {}
 
   ngOnInit(): void {
+    this.loading$ = this._store.select(selectitemsLoading);
+
     this._store.dispatch(loadItems());
+
+    this._showCaseSvc.getDataApi().subscribe((res: ItemModel[]) => {
+      this._store.dispatch(loadItemsSuccess({ items: res }));
+    });
   }
 }
