ab721e279590e544f8bb4f2755647643461d3ba1
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-12-09 03:53:11 -0300

    update component ui block

--- src/app/modules/show-case/components/ui-block-item/ui-block-item.component.ts
+++ src/app/modules/show-case/components/ui-block-item/ui-block-item.component.ts
@@ -1,28 +1,22 @@
-
+import { selectListItems } from './../../../../state/selectors/itesm.selectors';
 import { Component, Input, OnInit } from '@angular/core';
+import { ItemModel } from '@core/models/Item.interface';
 import { ShowCaseService } from '@modules/show-case/services/show-case.service';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { AppState } from 'src/app/state/app.state';
 
 @Component({
   selector: 'app-ui-block-item',
   templateUrl: './ui-block-item.component.html',
-  styleUrls: ['./ui-block-item.component.css']
+  styleUrls: ['./ui-block-item.component.css'],
 })
 export class UiBlockItemComponent implements OnInit {
+  items$: Observable<readonly ItemModel[]> = new Observable();
 
-  listData: any = []
-
-  constructor(private showCase: ShowCaseService) {
-
-
-  }
+  constructor(private _store: Store<AppState>) {}
 
   ngOnInit(): void {
-    // this.showCase.getDataApi()
-    //   .subscribe((res) => {
-    //     this.listData = res
-    //   })
+    this.items$ = this._store.select(selectListItems)
   }
-
-
 }
