3ee2edaf95bff190dbf3c33bdde9cb4dce734780
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-12-09 03:52:27 -0300

    add reducer

---
+++ src/app/state/app.reducers.ts
@@ -0,0 +1,10 @@
+import { ActionReducerMap } from '@ngrx/store';
+import * as reducers from './reducers';
+
+export interface AppState {
+  items: reducers.ItemState;
+}
+
+export const appReducers: ActionReducerMap<AppState> = {
+  items: reducers.itemsReducer,
+};
