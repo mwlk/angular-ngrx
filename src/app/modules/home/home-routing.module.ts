ec0ac6557ad8c1aeaa27482faa57e3e1760fd6aa
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-12-09 03:52:54 -0300

    update root route

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,17 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'show-case',
+    pathMatch: 'full',
+  },
+  {
     path: 'show-case',
-    loadChildren: () => import('@modules/show-case/show-case.module').then((m) => m.ShowCaseModule)
+    loadChildren: () =>
+      import('@modules/show-case/show-case.module').then(
+        (m) => m.ShowCaseModule
+      ),
   },
   {
     path: 'builder',
-    loadChildren: () => import('@modules/builder/builder.module').then((m) => m.BuilderModule)
-  }
+    loadChildren: () =>
+      import('@modules/builder/builder.module').then((m) => m.BuilderModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
